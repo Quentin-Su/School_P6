a51ee07c047f109a75c0269f6a88c7d6ea69cc88
Quentin-Su <Quentin.Sussetto@outlook.fr>
2023-03-10 23:46:30 +0100

    Footer & Layout Components

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import { ReactComponent as Icon } from '../utils/assets/kasaIcon.svg';
+
+export const Footer = () => {
+  return (
+    <footer className='footer_container'>
+      <div className='footer_content'>
+        <Icon className='footer_icon' />
+        <span className='footer_copy'>&copy; {new Date().getFullYear()} Kasa. All rights reserved</span>
+      </div>
+    </footer>
+  );
+};
