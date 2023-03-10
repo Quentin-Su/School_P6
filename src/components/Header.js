2c0ee0c6dbbf0e6b2653db8effd1d5058395cad7
Quentin-Su <Quentin.Sussetto@outlook.fr>
2023-03-10 23:46:07 +0100

    Header Component

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import { ReactComponent as Icon } from '../utils/assets/kasaIcon.svg';
+import { NavLink } from 'react-router-dom';
+
+export const Header = () => {
+  return (
+    <header className='header_container'>
+      <div className='header_content'>
+        <Icon className='header_icon' />
+        <nav className='header_nav'>
+          <ul>
+            <li>
+              <NavLink to='/' activeclassname='active' className='header_navLink'>Accueil</NavLink>
+            </li>
+            <li>
+              <NavLink to='/contact' activeclassname='active' className='header_navLink'>A Propos</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
