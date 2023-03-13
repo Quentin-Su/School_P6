c773a8a1734fe925c5db35698f4e902df4fb3656
Quentin-Su <Quentin.Sussetto@outlook.fr>
2023-03-13 23:24:14 +0100

    Card Component

---
+++ src/components/Card.js
@@ -0,0 +1,10 @@
+export const Card = ({ coverUrl, title }) => {
+    return (
+        <article className='card_item'>
+            <img src={coverUrl} alt='Card Location' />
+            <div className='card_item_content'>
+                <span className='card_item_title'>{title}</span>
+            </div>
+        </article>
+    );
+};
