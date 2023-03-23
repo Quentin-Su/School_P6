d474545d560b0e29453d74cbea72cb9e3b3ecd84
Quentin-Su <Quentin.Sussetto@outlook.fr>
2023-03-23 23:24:50 +0100

    About Page

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import { Banner } from '../components/Banner/Banner';
+import { Collapse } from '../components/Collapse/Collapse';
+import AboutContext from '../utils/context/aboutContext.json';
+import '../utils/styles/About.css';
+
+export const About = () => {
+    return (
+        <>
+            <Banner bannerClass='about' />
+            <section className='about_collapse'>
+                {
+                    AboutContext.map((value, index) => (
+                        <Collapse key={index} title={value.title} content={value.content} />
+                    ))
+                }
+            </section>
+        </>
+    );
+};
